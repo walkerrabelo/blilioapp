2c9d978a76794ff9b3f9e099876f9d79faf144ef
walker <walkerrabelo@gmail.com>
2021-02-27 04:57:30 -0300

    Adicionando "workaround" de dependencia do service

--- src/hooks/useList.js
+++ src/hooks/useList.js
@@ -4,10 +4,11 @@ const useList = (service) => {
     const [dados, setDados] = useState([])
     const [carregando, setCarregando] = useState(true)
     useEffect(() => service().then((resposta) => {
+        console.log('Calling useEffect...')
             setCarregando(false) 
             setDados(resposta.docs.map(dado => { return {id: dado.id, key: dado.id, ...dado.data() } }))
         }),
-    [])
+    [service])
     return [dados, carregando]
 }
 
